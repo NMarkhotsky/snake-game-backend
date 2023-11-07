ea869545520308ff1b17906f12602a013611b12b
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-11-07 14:45:00 +0200

    Initial backend

---
+++ db.js
@@ -0,0 +1,14 @@
+const { Pool } = require('pg');
+
+const { POSTGRES_URL } = process.env;
+
+const pool = new Pool({
+  connectionString: POSTGRES_URL + '?sslmode=require',
+});
+
+pool.connect((error) => {
+  if (error) throw error;
+  console.log('Success connection to DB');
+});
+
+module.exports = pool;
